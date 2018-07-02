fa021619ccccb398ac0188312a5cee32e189f9b6
Miquel Postigo Llabres <miquelpostigo@gmail.com>
2018-07-02 18:44:49 +0200

    Folders reestructuration and new world Generation

---
+++ ReEntrega/Assets/Resources/#Scripts/RoadGenerator/RoadManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[System.Serializable]
+public class TilesDictionary
+{
+    public int zone;
+    public GameObject tile;
+
+    public TilesDictionary(int z, GameObject t)
+    {
+        zone = z;
+        tile = t;
+    }
+}
+
+public class RoadManager : MonoBehaviour {
+
+    [SerializeField]
+    public List <TilesDictionary> tilesList;
+    // Path where the Resources folder is located
+    public string resourcesPath;
+    // Prefab folders path must end with /
+    public string [] prefabFolders;
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i < prefabFolders.Length; i++)
+        {
+            DirectoryInfo info = new DirectoryInfo(resourcesPath + prefabFolders[i]);
+            FileInfo [] fileInfo = info.GetFiles();                       
+
+            foreach (FileInfo file in fileInfo)
+            {
+                if (file.ToString().Contains(".prefab") && !file.ToString().Contains(".meta"))
+                {
+                    string prefabPath = "" + prefabFolders[i] + file.Name.Remove(file.Name.IndexOf('.'));
+                    Debug.Log(prefabPath);
+                    tilesList.Add(new TilesDictionary(i,(GameObject)Resources.Load(prefabPath, typeof(GameObject))));
+                }
+                
+            }
+        }        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
