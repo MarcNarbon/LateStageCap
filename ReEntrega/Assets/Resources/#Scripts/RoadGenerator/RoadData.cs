ac6428af2ffd91e24f13766465e5fd7d582a16f6
Miquel Postigo Llabres <miquelpostigo@gmail.com>
2018-07-04 14:27:42 +0200

    Tiles generation started

---
+++ ReEntrega/Assets/Resources/#Scripts/RoadGenerator/RoadData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ROAD_TYPE { CENTER, CENTER_LEFT, CENTER_RIGHT, SIDE_LEFT, SIDE_RIGHT, LIMIT_LEFT, LIMIT_RIGHT };
+
+public class RoadData : MonoBehaviour
+{
+    public ROAD_TYPE roadType;
+}
--- ReEntrega/Assets/Resources/#Scripts/RoadGenerator/RoadManager.cs
+++ ReEntrega/Assets/Resources/#Scripts/RoadGenerator/RoadManager.cs
@@ -3,27 +3,40 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-[System.Serializable]
-public class TilesDictionary
-{
-    public int zone;
-    public GameObject tile;
+public enum ZONE { INITIAL };
 
-    public TilesDictionary(int z, GameObject t)
+[System.Serializable]
+public class TileInfo
+{
+    public ZONE zone;
+    public ROAD_TYPE rType;
+    public GameObject tile;
+    public Vector2 dimensions = new Vector2();
+
+    public TileInfo(int z, GameObject t)
     {
-        zone = z;
+        zone = (ZONE)z;
         tile = t;
+        rType = t.GetComponent<RoadData>().roadType;
+        dimensions = t.GetComponent<BoxCollider2D>().size * t.transform.localScale;
     }
 }
 
 public class RoadManager : MonoBehaviour {
 
     [SerializeField]
-    public List <TilesDictionary> tilesList;
+    private List <TileInfo> tilesList;
+
+    [Space(10)]
     // Path where the Resources folder is located
     public string resourcesPath;
     // Prefab folders path must end with /
     public string [] prefabFolders;
+
+    [Space(10)]
+    [SerializeField]
+    private int currentZone = 0;
+
 	// Use this for initialization
 	void Start () {
         for(int i = 0; i < prefabFolders.Length; i++)
@@ -37,15 +50,60 @@ public class RoadManager : MonoBehaviour {
                 {
                     string prefabPath = "" + prefabFolders[i] + file.Name.Remove(file.Name.IndexOf('.'));
                     Debug.Log(prefabPath);
-                    tilesList.Add(new TilesDictionary(i,(GameObject)Resources.Load(prefabPath, typeof(GameObject))));
+                    tilesList.Add(new TileInfo(i,(GameObject)Resources.Load(prefabPath, typeof(GameObject))));
                 }
                 
             }
-        }        
+        }
+
+        foreach (TileInfo x in tilesList)
+        {
+            if(x.zone == (ZONE)currentZone)
+            {
+                GameObject holder = Instantiate(x.tile, transform);
+                Vector2 position = SpawnPosition(x);
+                holder.transform.position = new Vector3(position.x, position.y, 0f);
+            }
+        }
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    Vector2 SpawnPosition(TileInfo tileInfo)
+    {
+        Vector2 position = new Vector2();
+
+        switch (tileInfo.rType)
+        {
+            case ROAD_TYPE.CENTER:
+                position.x = 0;
+                break;
+            case ROAD_TYPE.CENTER_LEFT:
+                position.x -= tileInfo.dimensions.x;
+                break;
+            case ROAD_TYPE.CENTER_RIGHT:
+                position.x += tileInfo.dimensions.x;
+                break;
+            case ROAD_TYPE.SIDE_LEFT:
+                position.x -= tileInfo.dimensions.x * 2;
+                break;
+            case ROAD_TYPE.SIDE_RIGHT:
+                position.x +=  tileInfo.dimensions.x * 2;
+                break;
+            case ROAD_TYPE.LIMIT_LEFT:
+                position.x -= tileInfo.dimensions.x * 2;
+                break;
+            case ROAD_TYPE.LIMIT_RIGHT:
+                position.x += tileInfo.dimensions.x * 2;
+                break;
+            default:
+                break;
+        }
+
+        return position;
+    }
 }
